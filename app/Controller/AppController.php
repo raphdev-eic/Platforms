099a614252fe48a4ddf7f0963de56c9242296b87
rapahel Blehoue <raphaelsampan@outlook.fr>
2014-01-18 12:05:02 +0000

    modification des pages pour l'auth systeme
    pour ameliore l'authentification

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -3,14 +3,9 @@ App::uses('Controller', 'Controller');
 class AppController extends Controller {
 
   public $helpers = array('Form', 'Html', 'Js', 'Time','Image.Image');
-  public $components = array('Session',
-      'Cookie'=>array(
-            'name'=>'EicCorporation',
-            'domain'=>'.eic-corporation.com',
-            'path'=>'/',
-            'key'=>'qSI232qs*&11~_+!@#HKAv!@*(XSL#$%)asGb$@is~#sXOw!adre@34S^',
-            'httpOnly'=>true
-      ),
+  public $components = array(
+      'Session',
+      'Cookie',
       'Auth'=>array(
         'authenticate' => array(
             'Form' => array(
@@ -24,31 +19,50 @@ class AppController extends Controller {
 
 	public function beforeFilter(){
        parent::beforeFilter();
-       if($this->RequestHandler->isAjax()){
-           $this->layout = false;
-           Configure::write('debug',0);
-                if($this->request->is('ajax')){
-                    $this->disableCache();
-                }
-       }
-       $this->Auth->logoutRedirect = Configure::read('apps');
-       if($this->Cookie->check('EicAuth') && !$this->Auth->loggedIn()){
-          $this->loadModel('User');
-          //je charge le model
-          // recherche un User qui a ce cookie dans la bd
-         $user = $this->User->find('first', array(
-                'conditions' => array('User.id' => $this->Cookie->read('EicAuth'))
-            ));
-        if(!empty($user)){
-           $this->Auth->login($user);
-           $this->Cookie->write('EicAuth', $this->Cookie->read('EicAuth'),true, time() + 3600 * 24 * 3);
-          }else{
-           $this->Cookie->write('EicAuth','',true, time() - 3600 * 24 * 3);
-           $this->Auth->loginAction = Configure::read('accounts');
-          }
+
+       // parametrage du cookie
+
+        $this->Cookie->name ='EicCorporation';
+        $this->Cookie->domain ='.eic-corporation.com';
+        $this->Cookie->path = '/';
+        $this->Cookie->time = '3 Days';
+        $this->Cookie->key = 'qSI232qs*&11~_+!@#HKAv!@*(XSL#$%)asGb$@is~#sXOw!adre@34S^';
+        $this->Cookie->httpOnly = true;
+
+        $this->Auth->loginAction = Configure::read('accounts');
+
+        //detection d'une requete en ajax
+
+        if($this->RequestHandler->isAjax()){
+             $this->layout = false;
+             Configure::write('debug',0);
+                  if($this->request->is('ajax')){
+                      $this->disableCache();
+                  }
+        }
+
+        // detection de cookie
+
+       if($this->Cookie->check('eagleinvestkey') && !$this->Auth->loggedIn()){
+            $this->loadModel('User'); //je charge le model
+
+            // recherche un User qui a ce cookie dans la bd
+
+           $user = $this->User->find('first', array(
+                  'conditions' => array('User.id' => $this->Cookie->read('eagleinvestkey'))
+              ));
+
+            if(!empty($user)){
+                    $this->Auth->login($user);
+                    $this->Cookie->write('eagleinvestkey', $this->Cookie->read('eagleinvestkey'),true, time() + 3600 * 24 * 3);
+              }else{
+                    $this->Cookie->destroy();
+                    $this->redirect(Configure::read('accounts'));
+              }
        }else{
-         $this->Auth->loginAction = Configure::read('accounts');
+        // Au cas ou le cookie n'existe pas
+           $this->redirect(Configure::read('accounts'));
        }
-	  }
+	}
 
 }
--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -1,74 +1,93 @@
 <?php
 App::uses('AppController','Controller');
-class UsersController extends AppController{
+class UsersController extends AppController {
 
 	public function beforeFilter(){
 		parent::beforeFilter();
 	}
 
-  /**
-   * function d"affichage du profil en cours
-   * @return [type] [description]
-   */
+/**
+ * function d"affichage du profil en cours
+ * @return [type] [description]
+ */
+
 	public function index(){
-		$user_id = $this->Auth->user('User.id');
-		if(!$user_id){
-			$this->redirect(Configure::read('apps'));
-		}
-		$parts = $this->User->find('first',array(
-         'conditions'=>array('User.id'=>$user_id),
-         'fields'=>array('User.parts')
-		));
-		$this->loadModel('Value');
-		$vl = $this->Value->find('first',array(
-           'order'=>array('Value.created Desc')
-		));
-    //recuperation des dernier inscrit
-    $lastSign = $this->User->getLastSign();
-    $count = $this->User->CountChild($user_id);
-		$this->set(compact('vl','parts','count','lastSign'));
+       //detecté si une authentifaication existe bien
+    		$user_id = $this->Auth->user('User.id');
+    		if(!$user_id){
+    			$this->redirect(Configure::read('accounts'));
+    		}
+
+        //recuperation du nombre de parts
+
+    		$parts = $this->User->find('first',array(
+             'conditions'=>array('User.id'=>$user_id),
+             'fields'=>array('User.parts')
+    		));
+
+         //recupration de la valeur liquidatif du moment
+
+    		$this->loadModel('Value');
+    		$vl = $this->Value->find('first',array(
+               'order'=>array('Value.created Desc')
+    		));
+
+      //recuperation des derniers inscrits
+
+      $lastSign = $this->User->getLastSign();
+
+     // compte le nombre de fieulle de l'utilisateurs this
+
+      $count = $this->User->CountChild($user_id);
+
+
+  		$this->set(compact('vl','parts','count','lastSign'));
 	}
+
 
 /**
  * fonction d'affichage de profil
  */
 
     public function profil(){
-    $user_id = $this->Auth->user('User.id');
-       if(!$user_id){
-		    $this->Cookie->write('EicAuth','',true, time() - 3600 * 24 * 3);
-		    $this->Cookie->delete('EicAuth');
-		    return $this->redirect($this->Auth->logout());
-       }
-       $userInfo = $this->User->getUserInfo($user_id);
-    		$this->loadModel('Value');
-    		$vl = $this->Value->find('first',array(
-               'order'=>array('Value.created Desc')
-    		));
-        $avatar = $this->User->Avatar->find('all',array(
-         'conditions'=>array('User.id'=>$this->Auth->user('User.id')),
-         'order'=>'Avatar.id Desc',
-         'limit'=>1
-        ));
-       $avatar = current($avatar);
-       /*debug($avatar);
-       die();*/
-       $this->set(compact('userInfo','vl','avatar'));
+
+           $user_id = $this->Auth->user('User.id');
+           if(!$user_id){
+        		    $this->Cookie->destroy();
+        		    $this->redirect(Configure::read('accounts'));
+           }
+           $userInfo = $this->User->getUserInfo($user_id);
+        	 $this->loadModel('Value');
+        	 $vl = $this->Value->find('first',array(
+                   'order'=>array('Value.created Desc')
+        	 ));
+
+           $avatar = $this->User->Avatar->find('all',array(
+            'conditions'=>array('User.id'=>$this->Auth->user('User.id')),
+            'order'=>'Avatar.id Desc',
+            'limit'=>1
+           ));
+
+           $avatar = current($avatar);
+           /*debug($avatar);
+           die();*/
+           $this->set(compact('userInfo','vl','avatar'));
     }
 
-    /**
-     * function d'edition de profil
-     */
+/**
+ * function d'edition de profil
+ */
 
     public function edit($id = null){
-       $user_id = $this->Auth->user('User.id');
-       if(!$user_id){
-		    $this->Cookie->write('EicAuth','',true, time() - 3600 * 24 * 3);
-		    $this->Cookie->delete('EicAuth');
-		    return $this->redirect($this->Auth->logout());
-       }
-       $userInfo = $this->User->getUserInfo($user_id);
-       $this->set(compact('userInfo'));
+           $user_id = $this->Auth->user('User.id');
+
+           if(!$user_id){
+                $this->Cookie->destroy();
+                $this->redirect(Configure::read('accounts'));
+           }
+
+           $userInfo = $this->User->getUserInfo($user_id);
+           $this->set(compact('userInfo'));
     }
 
 /**
@@ -77,20 +96,25 @@ class UsersController extends AppController{
 
     public function ListFeuil(){
       	if(!$this->Auth->user('User.id')){
-      	   	 $this->redirect(Configure::read('apps'));
+      	   	 $this->redirect(Configure::read('accounts'));
       	}
+
         $list = $this->User->find('first',array(
-         'contain'=>array('ChildUser','Rank','City','Avatar'),
-         'conditions'=>array('User.id'=>$this->Auth->user('User.id')),
-         'order'=>'User.firstname Asc'
+           'contain'=>array('ChildUser','Rank','City','Avatar'),
+           'conditions'=>array('User.id'=>$this->Auth->user('User.id')),
+           'order'=>'User.firstname Asc'
         ));
+
         $fieul = $list['ChildUser'];
+
         foreach ($fieul as $key => $v){
           $id[] = $v['id'];
         }
-        if(!empty($id)){
 
+        if(!empty($id)){
+           $id[] = 0;
         }
+
         $arr = $this->User->find('all',array(
           'contain'=>array('Avatar'),
           'fields'=>array('User.id','User.firstname','User.lastname','User.email'),
@@ -102,40 +126,48 @@ class UsersController extends AppController{
         $this->set(compact('arr','count_child'));
     }
 
-	/**
-	 * fonction d'ajout de feuil
-	 */
+/**
+ * fonction d'ajout de feuil
+ */
 
 	public function AddFeuil(){
       	if(!$this->Auth->user('User.id')){
-      	   	 $this->redirect(Configure::read('apps'));
+      	   	 $this->redirect(Configure::read('accounts'));
       	}
-      // add function fieul
+
+        //add function fieulle
+
         if(!empty($this->request->data) && $this->request->is('post')){
+
         	 //affectation du champ parent_id
+
         	   $this->request->data['User']['parent_id'] = $this->Auth->user('User.id');
+
         	   //creer un nouvelle enregistrement en attentes
-              $this->User->create();
-  	            if($this->User->save($this->request->data,true,array('parent_id','firstname','email','lastname'))){
-  		       	   $this->Session->write('fieul_id',$this->User->id);
-  			    //envoi d'email
-  			       $this->User->send($this->request->data['User']);
-  	               $this->Session->write('fieul_id',$this->User->id);
-  		      	   $this->Session->setFlash('Enregistrement  effectué avec success , un mail à été envoyé au fieul, Vous pouvez joindre le Bordero du fieul si vous l\'avez scanner','success');
+
+             $this->User->create();
+
+  	          if($this->User->save($this->request->data,true,array('parent_id','firstname','email','lastname'))){
+
+              //on met l'id du fieul en session
+  		       	  $this->Session->write('fieul_id',$this->User->id);
+  			      //envoi d'email
+  			        $this->User->send($this->request->data['User']);
+
+  		      	   $this->Session->setFlash('Enregistrement  effectué avec success , un mail à été envoyé au fieulle, Vous pouvez joindre le Bordero du fieul si vous l\'avez scanner','success');
   		      	   $this->redirect(array('controller'=>'Borderos','action'=>'AddFieulBordero'));
   		        }
-  	            $this->Session->setFlash('Warning! Enregistrement Du fieul echoué ,  veuillez réessayer','error');
+  	          $this->Session->setFlash('Warning! Enregistrement Du fieulle echoué ,  veuillez réessayer','error');
   	    }
-    }
+ }
 
-    /**
-     * [logout description]
-     * @return [type] [description]
-     */
+/**
+ * [logout description]
+ * @return [type] [description]
+ */
 
 	  public function logout(){
-	    $this->Cookie->write('EicAuth','',true, time() - 3600 * 24 * 3);
-	    $this->Cookie->delete('EicAuth');
-	    return $this->redirect($this->Auth->logout());
+	    $this->Cookie->destroy();
+	    $this->redirect($this->Auth->logout());
     }
 }
